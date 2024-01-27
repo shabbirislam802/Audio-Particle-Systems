f83c2e7c15a7a9bd3adde8861f87d496ec0e3e5f
shabbirislam802 <149245820+shabbirislam802@users.noreply.github.com>
2024-01-27 10:04:09 +0100

    update of scene

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 _offset;
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothTime;
+    private Vector3 _currentVelocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake() => _offset = transform.position - target.position;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + _offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
+    }
+
+}
